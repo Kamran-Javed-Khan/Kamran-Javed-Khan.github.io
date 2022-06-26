706d62cdd62213f7dddcfb6b741fd01440702102
Kamran.Javed.Khan <kjk.secondary@gmail.com>
2022-06-26 19:14:17 +0530

    final commit

--- script.js
+++ script.js
@@ -4,12 +4,16 @@ let adviceIdSection = document.querySelector(".advice-id");
 let adviceText = '';
 let adviceTextSection = document.querySelector(".advice-text");
 
-fetch("https://api.adviceslip.com/advice")
-.then(response => response.json())
-.then(data => {
-    adviceId = data?.slip?.id;
-    adviceIdSection.textContent = adviceIdSection.textContent.replace("#", "#" + adviceId);
+function getSomeAdvice() {
+    fetch("https://api.adviceslip.com/advice")
+    .then(response => response.json())
+    .then(data => {
+        adviceId = data?.slip?.id;
+        adviceIdSection.textContent = adviceIdSection.textContent.replace(adviceIdSection.textContent, "Advice #" + adviceId);
 
-    adviceText = data?.slip?.advice;
-    adviceTextSection.textContent = adviceTextSection.textContent.replace("\"\"", "\"" + adviceText + "\"");
-});
+        adviceText = data?.slip?.advice;
+        adviceTextSection.textContent = adviceText;
+    });
+}
+
+getSomeAdvice();
